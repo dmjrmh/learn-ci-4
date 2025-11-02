b06fc7461b824a0cb8185dc6c2d0db5cda7981ab
dmjrmh <gban1mj27@gmail.com>
2025-11-02 11:45:30 +0700

    added faker for user seeder

--- app/Database/Seeds/UserSeeder.php
+++ app/Database/Seeds/UserSeeder.php
@@ -9,26 +9,37 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-      $data = [
-        [
-          'name'       => 'Arik Agustino',
-          'address'    => 'Jl. Merdeka No. 100, Jombang',
-          'created_at' => Time::now(),
+      // $data = [
+      //   [
+      //     'name'       => 'Arik Agustino',
+      //     'address'    => 'Jl. Merdeka No. 100, Jombang',
+      //     'created_at' => Time::now(),
+      //     'updated_at' => Time::now(),
+      //   ],
+      //   [
+      //     'name'       => 'Caesarik',
+      //     'address'    => 'Jl. Semangka No. 28, Bandung',
+      //     'created_at' => Time::now(),
+      //     'updated_at' => Time::now(),
+      //   ],
+      //   [
+      //     'name'       => 'Kolik Manjiro',
+      //     'address'    => 'Jl. Jeruk No. 77, Yogyakarta',
+      //     'created_at' => Time::now(),
+      //     'updated_at' => Time::now(),
+      //   ],
+      // ];
+
+      $faker = \Faker\Factory::create('id_ID');
+      for($i =0; $i < 100; $i++){
+        $data = [
+          'name' => $faker->name(),
+          'address' => $faker->address(),
+          'created_at' => Time::createFromTimestamp($faker->unixTime()),
           'updated_at' => Time::now(),
-        ],
-        [
-          'name'       => 'Caesarik',
-          'address'    => 'Jl. Semangka No. 28, Bandung',
-          'created_at' => Time::now(),
-          'updated_at' => Time::now(),
-        ],
-        [
-          'name'       => 'Kolik Manjiro',
-          'address'    => 'Jl. Jeruk No. 77, Yogyakarta',
-          'created_at' => Time::now(),
-          'updated_at' => Time::now(),
-        ],
-      ];
+        ];
+        $this->db->table('users')->insert($data);
+      }
 
       // simple queries
       // $this->db->query(
@@ -37,6 +48,6 @@ class UserSeeder extends Seeder
 
       // query builder
       // $this->db->table('users')->insert($data);
-      $this->db->table('users')->insertBatch($data);
+      // $this->db->table('users')->insertBatch($data);
     }
 }
